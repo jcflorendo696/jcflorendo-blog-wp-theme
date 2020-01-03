25893ba2a0deb550b906364958cae62f4fe72cf9
Jc <jc.florendo696@gmail.com>
2020-01-04 07:20:31 +0800

    Added objectives.txt.

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+
+
+<div class="container pb-4">
+    <div class="row">
+        <div class="col-12 text-center">
+            <h1>OOPS!</h1>
+            <h3>Page Not Found!</h3>
+        </div>    
+    </div>
+</div>
+
+
+<?php get_footer(); ?>
--- components/the-loop.php
+++ components/the-loop.php
@@ -6,22 +6,26 @@ This is a "The Loop" template. It lists all posts showing the following:
  - Author
  - Categories
  - with margin-bottom spacings
+
+You can call this component everywhere in the theme if you want a loop. Just modify the template with conditions.
 */
 ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
+    <div class="card p-5 mb-4">
+        <h3><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h3>
+        <div class="meta-details">
+            <span class='date'><?php the_date(); ?> - <?php the_time(); ?></span> | By <span><?php the_author_posts_link(); ?></span> | <span><?php the_category(); ?></span>
+        </div>
+        <p><?php the_excerpt(); ?></p>
 
 
+        <?php if( !is_single() ):?>    
+            <p><?php the_shortlink('Read More >'); ?></p>
+        <?php endif; ?>
 
-<div class="card p-5 mb-4">
-    <h3><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h3>
-    <div class="meta-details">
-        <span class='date'><?php the_date(); ?> - <?php the_time(); ?></span> | By <span><?php the_author_posts_link(); ?></span> | <span><?php the_category(); ?></span>
+        
     </div>
-    <p><?php the_excerpt(); ?></p>
-
-    <p><?php the_shortlink('Read More >'); ?></p>
-</div>
 
 <?php endwhile; ?>
 <?php endif; ?>
